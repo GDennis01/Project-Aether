b8c27054af5110d570caae36452a01a0515614e5
GDennis01 <dennis.gobbi001@gmail.com>
2025-06-08 03:22:04 +0200

    fixed min size

--- scripts/world_manager.gd
+++ scripts/world_manager.gd
@@ -4,4 +4,4 @@ extends WorldEnvironment
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var _window := get_window()
-	_window.min_size = Vector2i(1330, 930)
+	_window.min_size = Vector2i(1330, 980)
