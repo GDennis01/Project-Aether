f8dac9f1ee324a26f0213a55e0e71a95e8752ca3
GDennis01 <dennis.gobbi001@gmail.com>
2025-10-15 23:30:54 +0200

    fixed window resize bug for WEB export

--- scripts/world_manager.gd
+++ scripts/world_manager.gd
@@ -2,6 +2,16 @@ extends WorldEnvironment
 #@onready var HUD = $HUD
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+# func _ready() -> void:
+# 	var _window := get_window()
+# 	_window.min_size = Vector2i(1330, 980)
+func _ready():
+	# Wait a short moment, then force a resize
+	await get_tree().process_frame
+	_force_resize()
 	var _window := get_window()
 	_window.min_size = Vector2i(1330, 980)
+
+func _force_resize():
+	var size = DisplayServer.window_get_size()
+	DisplayServer.window_set_size(size)
